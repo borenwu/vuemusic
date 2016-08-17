ec68db3fee7cb9ef17debcdc1d9a4feba4a027d2
borenwuapple@163.com <borenwuapple@163.com>
2016-08-17 22:47:00 +0800

    first edtion

---
+++ static/js/src/components/player/player.vue
@@ -0,0 +1,32 @@
+<template>
+    <link rel="stylesheet" href="static/js/src/components/player/meplayer.min.css">
+    <div id="player1" class="music"></div>
+</template>
+<style>
+
+</style>
+
+<script>
+    import meplayer from 'E:\\python\\web\\flask\\music\\static\\js\\src\\components\\player\\meplayer.min.js'
+    export default{
+        data(){
+            return {
+                msg: 'hello vue'
+            }
+        },
+
+        ready (){
+            mePlayer({
+                music: {
+                    src: 'static/music/haohao.mp3',
+                    title: '好好',
+                    author: 'MAY DAY',
+                    cover: 'static/img/haohao.jpg',
+                },
+                target: '.music'
+            });
+
+        }
+
+    }
+</script>
--- static/js/src/index.js
+++ static/js/src/index.js
@@ -3,10 +3,12 @@
  */
 var Vue = require('vue')
 var App = require('./components/app.vue')
+var Player = require('./components/player/player.vue')
 
 var main = new Vue({
     el: '#example',
     components: {
         app: App,
+        player:Player
     }
 })
